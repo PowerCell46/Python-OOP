23d49154af55239cc06536bce31d1428be3ff213
Peter Gerdzhikov <116914391+PowerCell46@users.noreply.github.com>
2023-07-04 23:23:04 +0300

    Delete Hierarchical Inheritance directory

--- Hierarchical Inheritance/cat.py
+++
@@ -1,8 +0,0 @@
-from project.animal import Animal
-
-class Cat(Animal):
-    def meow(self):
-        return f'meowing...'
-
-# pisana = Cat()
-# print(pisana.eat())
--- Hierarchical Inheritance/dog.py
+++
@@ -1,8 +0,0 @@
-from project.animal import Animal
-
-class Dog(Animal):
-    def bark(self):
-        return f'barking...'
-
-# sharo = Dog()
-# print(sharo.eat())
