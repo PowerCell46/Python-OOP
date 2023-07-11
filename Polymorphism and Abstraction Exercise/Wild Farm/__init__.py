9d0784388a15dc59798219fcaa09a63e664b5066
Peter Gerdzhikov <116914391+PowerCell46@users.noreply.github.com>
2023-07-11 23:20:32 +0300

    Add files via upload

---
+++ Polymorphism and Abstraction Exercise/Wild Farm/__init__.py
@@ -0,0 +1,27 @@
+# from project.animals.animal import Mammal
+# from project.animals.birds import Owl
+# from project.food import Vegetable
+# from project.food import Meat
+# from project.animals.birds import Hen
+# from project.food import Fruit
+#
+#
+# hen = Hen("Harry", 10, 10)
+#
+# veg = Vegetable(3)
+#
+# fruit = Fruit(5)
+#
+# meat = Meat(1)
+#
+# print(hen)
+#
+# print(hen.make_sound())
+#
+# hen.feed(veg)
+#
+# hen.feed(fruit)
+#
+# hen.feed(meat)
+#
+# print(hen)
