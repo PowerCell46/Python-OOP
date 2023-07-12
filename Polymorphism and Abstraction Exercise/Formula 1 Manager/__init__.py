642aacb2c7676f8a0d07c42f079bb2e7d5128473
Peter Gerdzhikov <116914391+PowerCell46@users.noreply.github.com>
2023-07-12 23:48:31 +0300

    Add files via upload

---
+++ Polymorphism and Abstraction Exercise/Formula 1 Manager/__init__.py
@@ -0,0 +1,17 @@
+# from project.dog import Dog
+# from project.tomcat import Tomcat
+# from project.cat import Cat
+# from project.kitten import Kitten
+#
+# kitten = Kitten("Kiki", 1)
+#
+# print(kitten.make_sound())
+#
+# print(kitten)
+#
+# cat = Cat("Johnny", 7, "Male")
+#
+# print(cat.make_sound())
+#
+# print(cat)
+# print(cat)
